7b174545d69ab8e27d8950ed00f2bb3dfc2ebb52
Pedro Munhoz <pedr.munhoz@gmail.com>
2016-06-20 17:11:41 -0300

    cities add in a dinamic form from the xml

--- my_script.js
+++ my_script.js
@@ -14,27 +14,31 @@ var my_xml;
 var jsonText;
 
 
-function loadDataToHTML(){
+function loadDataToHTML(page){
    // extraia a 'location' do HTTP GET ou URI
-   var city = window.location.search.substr(10,1000);
-   if (city === ""){
-      city = 0;
-   }
+   if(page){
+      var city = window.location.search.substr(10,1000);
+      if (city === ""){
+         city = 0;
+      }
 
-   var data = cities[city];
-   if (data == undefined){
-      city = 0;
-      data = cities[city];
+      var data = cities[city];
+      if (data == undefined){
+         city = 0;
+         data = cities[city];
+      }
    }
 
    my_xml = oReq.responseXML;
    jsonText = JSON.stringify(xmlToJson(my_xml));
    attCities(jsonText);
 
-   document.getElementById("city_name").textContent = data["name"];
-   document.getElementById("city_max").textContent = data["max"];
-   document.getElementById("city_min").textContent = data["min"];
-   //document.getElementById("city_updated").textContent = data["updated"];
+   if(page){
+      document.getElementById("city_name").textContent = data["name"];
+      document.getElementById("city_max").textContent = data["max"];
+      document.getElementById("city_min").textContent = data["min"];
+      //document.getElementById("city_updated").textContent = data["updated"];
+   }
 }
 
 // Changes XML to JSON
@@ -79,7 +83,7 @@ function xmlToJson(xml) {
 function attCities(jt) {
    var x = JSON.parse(jt);
    var c;
-   for(var i=0;i<20;i++){
+   for(var i=0;i<x["previsao-cidades"].cidade.length;i++){
       c = true;
       for(var j=0; j<cities.counter;j++){
          if(cities[j]["name"] == x["previsao-cidades"].cidade[i]["@attributes"].nome){
@@ -97,24 +101,33 @@ function attCities(jt) {
          var art = document.getElementById("cities_list");
          if (art != null) {
             var new_button = document.createElement("A");
-            new_button.text = cities[cities.counter]["name"];
             new_button.href = "index.html?location=" + cities.counter;
-            art.add(new_button);
-            cities[cities.counter]["name"] = x["previsao-cidades"].cidade[i]["@attributes"].nome;
-            cities[cities.counter].max = x["previsao-cidades"].cidade[i]["temperatura-maxima"]["#text"];
-            cities[cities.counter].min = x["previsao-cidades"].cidade[i]["temperatura-minima"]["#text"];
+            var p = document.createElement("P");
+            p.className = "city_bt";
+            if (i==x["previsao-cidades"].cidade.length-1) {
+               p.id = "last_bt";
+            }
+            var tx = document.createTextNode(cities[cities.counter]["name"]);
+            p.appendChild(tx);
+            new_button.appendChild(p);
+            art.appendChild(new_button);
          }
          (cities.counter)++;
       }
    }
 }
 
+
+
 function load() {
-   console.log("dzxnthed");
-   var art = document.getElementById("cities_list");
-   if (art == null){
-      loadDataToHTML();
-      loadDataToHTML();
+   var t = document.getElementById("cities_list");
+   if (t == null){
+      loadDataToHTML(true);
+      loadDataToHTML(true);
+   }
+   else {
+      loadDataToHTML(false);
+      loadDataToHTML(false);
    }
 }
 
